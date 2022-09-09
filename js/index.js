9d6952b42895c8d419c059750839515c005c81f8
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-09-09 20:27:05 +0530

    Local Storage implemented

--- js/index.js
+++ js/index.js
@@ -1,24 +1,55 @@
+var notes = []
+var notesContainer = document.getElementById("notesContainer")
+var fab = document.getElementById("btnFab")
+
+populateSavedNotes()
+
+function populateSavedNotes(){
+    notes = getSavedNotes()
+    notes.forEach(
+        (note) => renderNote(note)
+    )
+}
+
 function getNote(note) {
-    return `<div id = "${note.id}" class = "div__note">
+    return `<div id = "${note.noteId}" class = "div__note">
                 <h3 class = "note__title" >${note.title}</h3>
                 <p class = "note__body" >${note.body}</p>
             </div>`
 }
 
-var notes = [...Array(10).keys()].map(
-    (i) => {
-        return {
-            id: i,
-            title: `Title ${i}`,
-            body: `This is a test note ${i}`
-        }
+function createNoteFromId(i){
+    return {
+        noteId: i,
+        title: `Title ${i}`,
+        body: `This is a test note ${i}`
     }
-)
+}
 
-var notesContainer = document.getElementById("notesContainer")
+function createRandomNote(){
+    var randNum = Math.round(Math.random()*10)
+    var newNode = createNoteFromId(randNum)
+    console.log(newNode)
+    addNewNote(newNode)
+}
 
-notes.forEach(
-    (note)=>{
-        notesContainer.innerHTML+=getNote(note)+"\n"
-    }
-)
+function addNewNote(note){
+    notes.push(note)
+    console.log(notes.length)
+    reSaveAllNotesLocally()
+    renderNote(note)
+}
+
+function renderNote(note){
+    notesContainer.innerHTML+=getNote(note)+"\n"
+}
+
+function reSaveAllNotesLocally(){
+    window.localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+function getSavedNotes(){
+    return JSON.parse(window.localStorage.getItem("notes"))
+}
+
+fab.addEventListener('click', createRandomNote)
