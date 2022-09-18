408bb3ee9c6d35bc1eb6c33163a13f73314036ce
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-09-18 16:59:09 +0530

    Adding note feature added via Modal

--- js/index.js
+++ js/index.js
@@ -1,55 +1,102 @@
 var notes = []
 var notesContainer = document.getElementById("notesContainer")
 var fab = document.getElementById("btnFab")
+var modal = document.getElementById("createNoteModal")
 
 populateSavedNotes()
 
-function populateSavedNotes(){
-    notes = getSavedNotes()
+function populateSavedNotes() {
+    notes = getSavedNotes().sort((n1, n2) => n1.noteId - n2.noteId)
+    notesContainer.innerHTML = ""
     notes.forEach(
         (note) => renderNote(note)
     )
 }
 
 function getNote(note) {
-    return `<div id = "${note.noteId}" class = "div__note">
+    return `<div id = "${note.noteId}" onClick = "onNoteClicked(${note.noteId})" class = "div__note">
                 <h3 class = "note__title" >${note.title}</h3>
                 <p class = "note__body" >${note.body}</p>
             </div>`
 }
 
-function createNoteFromId(i){
+function createNoteFromId(i) {
     return {
         noteId: i,
-        title: `Title ${i}`,
-        body: `This is a test note ${i}`
+        title: ``,
+        body: ``
     }
 }
 
-function createRandomNote(){
-    var randNum = Math.round(Math.random()*10)
-    var newNode = createNoteFromId(randNum)
-    console.log(newNode)
-    addNewNote(newNode)
+function createNewNote() {
+    var id = 0
+    if (notes.length == 0) id = 0;
+    else id = notes[notes.length - 1].noteId + 1
+    var newNode = createNoteFromId(id)
+    return newNode
 }
 
-function addNewNote(note){
-    notes.push(note)
-    console.log(notes.length)
+function addNewNote(note) {
+    var index = notes.findIndex((n) => n.noteId === note.noteId )
+    if (index == -1) notes.push(note)
+    else notes[index] = note
+    console.log(note.noteId)
     reSaveAllNotesLocally()
-    renderNote(note)
 }
 
-function renderNote(note){
-    notesContainer.innerHTML+=getNote(note)+"\n"
+function renderNote(note) {
+    notesContainer.innerHTML += getNote(note) + "\n"
 }
 
-function reSaveAllNotesLocally(){
+function reSaveAllNotesLocally() {
     window.localStorage.setItem("notes", JSON.stringify(notes))
+    populateSavedNotes()
 }
 
-function getSavedNotes(){
-    return JSON.parse(window.localStorage.getItem("notes"))
+function getSavedNotes() {
+    var notes = JSON.parse(window.localStorage.getItem("notes"))
+    return (notes == null) ? [] : notes
 }
 
-fab.addEventListener('click', createRandomNote)
+var openedNote = null
+
+function openModal(note) {
+    openedNote = note
+    document.getElementById("noteTitle").value = note.title
+    document.getElementById("noteContent").value = note.body
+    modal.style.display = "block"
+}
+
+function closeModal() {
+    modal.style.display = "none"
+    openedNote = null
+}
+
+fab.addEventListener('click', () => {
+    openModal(createNewNote())
+})
+
+document.getElementById("saveNoteBtn").addEventListener(`click`, onSaveBtnPressed)
+
+window.onclick = function (event) {
+    if (event.target == modal)
+        closeModal()
+}
+
+function onNoteClicked(noteId) {
+    var note = notes.find((n) => n.noteId === noteId)
+    openModal(note)
+}
+
+function onSaveBtnPressed() {
+    title = document.getElementById("noteTitle").value.trim()
+    content = document.getElementById("noteContent").value.trim()
+    if (title.length === 0 || content.length === 0)
+        alert("Values cannot be empty")
+    else {
+        openedNote.title = title
+        openedNote.body = content
+        addNewNote(openedNote)
+        closeModal()
+    }
+}
